e2395d941ffbc4553fdcb7196eb275e7bab68659
bijay <bijay1@.com>
2022-12-05 00:09:53 +0530

    loop assignment

---
+++ loop1.js
@@ -0,0 +1,105 @@
+////////////program to print 1 to 10 using whilw loop//////////////
+
+// let num=1
+// while(num <= 10 ){
+//     console.log(num)
+//     num=num+1
+// }
+
+
+////////////program to print 10 to 1 using whilw loop /////////////////
+
+// let num2=10
+// while(num2 >= 1){
+//     console.log(num2)
+//     num2=num2-1
+// }
+
+//////////program to print 5 to 15 using for loop /////////////////////
+
+
+// for (  let i=5; i <= 15 ;i=i+1 ){
+//     console.log(i)
+// }
+
+
+//////////program to print 15 to 10 using for loop /////////////////////
+
+// for (let num4=15; num4 >= 10; num4=num4-1){
+//     console.log(num4)
+// }
+
+
+/////////program to print 1 to 10 even number using while loop ///////////////
+
+// let num5=2
+// while(num5 <= 10){
+//     console.log( num5)
+//     num5=num5+2
+//  }
+
+////////program to print 1 to 10 odd number using for loop //////////////////
+
+
+// for ( let num6=1; num6 <= 10; num6=num6+2){
+//     console.log(num6)
+// }
+
+//////////program first ten multiples of 4 using a while loop /////////////
+
+// let num7=4
+// while( num7 <= 4*10){
+//     console.log(num7)
+//     num7=num7+4
+// }
+
+
+//////////program to print a multiplication table of 6 using a while loop ///////////
+
+// let num8=6
+// while(num8 <= 6*10){
+//     console.log(num8)
+//     num8=num8+6
+// }
+
+///////////write a program facter  for 24 using while loop /////////////////
+
+// let num10=1
+// let inp=24
+//  while(num10 <= inp){
+//     if (inp % num10 == 0){
+//     console.log(num10)
+//     num10=num10 +1
+//     }
+//  }
+
+//////////program to print reverse of digits of number ////////////////////
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+
+
+///////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
