62ec84acee0bf501def5c97bb20477fe1476d7c3
bijay <bijay1@.com>
2022-12-14 22:22:34 +0530

    new

---
+++ objArray.js
@@ -0,0 +1,31 @@
+///////////////////CREATE & ACCESS ARRAYS OF  OBJECT/////////////////
+
+
+// let objj=[{
+//     id:101},
+//     {name:"bijay"},
+//     {add: "pdp"},
+//    ]
+
+// console.log(objj)
+// console.log(objj[1].name)
+
+///////////////////CREATE 4D ARRAY ///////////////////////
+// var ary=[
+//     ["harry",18,"mail","b.come"],
+//     ["bijay",28,"fmail","b.come"],
+//     ["barry",28,"mail","b.come"],
+//     [ "harry",38,"fmail","b.come"]
+// ];
+// for(var a=0;a<4;a++){
+//     for(var b=0; b<4; b++){
+//         document.write(ary[a][b] + " ");
+//     }
+//     document.write("<br>");
+// }
+
+//////////////--PUSH --POP--UNSHIFT--SHIFT--////////////////////
+
+let size=[10,20,30,40]
+size.shift() 
+console.log(size)
