75dfb57576c69fc31981211825b4a77980bbae37
bijay <bijay1@.com>
2022-12-15 13:05:54 +0530

    new2

---
+++ arrayslice.js
@@ -0,0 +1,50 @@
+////////////////splice////////////
+
+// let arr=[1,2,3,4,5]
+// arr.splice(2,2, "hi")
+// console.log(arr)
+
+///////////////slice//////////////////
+// let arr1=[1,2,3,4,5,6]
+
+// console.log( arr1.slice(1,3))
+
+///////////copy data from array ////////////////
+
+// var birds=["parrot","bijay", 'jacob']
+// var newbirds=[].concat(birds);
+// console.log(newbirds)
+
+/////////////copy //////////////
+
+// var birds=["parrot","bijay", 'jacob']
+//  var newbirds=[].concat(birds);
+//  newbirds.pop();
+//  console.log(newbirds)
+//  console.log(birds)
+
+///////////referance///////////////////
+
+// var birdss=["parrot","bijay", 'jacob']
+// var newbirdss=birdss
+// birdss.pop()
+// console.log(birdss)
+// console.log(newbirdss)
+
+///////////// indexOf////////////
+// var geek=["ranul","sonam","raj","sumit","raj"]
+// var position=geek.indexOf("raj")
+// console.log(position)
+
+///////////find object from array ///////////////
+
+// var geek=[ 
+//     {id: 1,name:"bijay" ,add:"pdp"},
+//     {id: 2,name:"jacob" ,add:"bbs"}
+// ]
+// let op=geek.find(val=>val.id===1);
+// console.log(op)
+
+
+
+
